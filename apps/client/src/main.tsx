c969d2c9660eeae39052a32da0df53ddad1ac507
peperoni4 <141176443+peperoni4@users.noreply.github.com>
2024-08-13 19:56:59 +0300

    feat: add channel header component

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/app/app.tsx';
+import './assets/css/common.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
